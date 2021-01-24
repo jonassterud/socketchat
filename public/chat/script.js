7a393741ef7089edd2c5935960df7ae591259a83
Jonas Sterud <57707537+jonassterud@users.noreply.github.com>
2021-01-24 23:01:18 +0100

    feat: visual improvement, send messages

--- public/chat/script.js
+++ public/chat/script.js
@@ -3,16 +3,26 @@
 const url = new URL(window.location.href);
 const ws = new WebSocket(`ws://${url.host}/ws${url.search}`);
 
-ws.onopen = (_) => {
-    ws.send("connected");
-}
-
 window.onload = () => {
-    const messages = document.querySelector("div#messages");
+    const messagesElement = document.querySelector("div#messages");
+    const sendButton = document.querySelector("input#send");
+    const message = document.querySelector("input#message");
 
     ws.onmessage = (ev) => {
         const evObj = JSON.parse(ev.data);
         const element = `<p class="message"><span class="author">${evObj.author}</span>: <span class="content">${evObj.message}</span><br><span class="time">${evObj.time}</span></p>`;
-        messages.innerHTML += element;
+        messagesElement.innerHTML += element;
+    }
+
+    ws.onopen = (_) => {
+        ws.send("connected");
+        sendButton.onclick = () => {
+            ws.send(message.value);
+        }
+    }
+
+    window.onkeydown = (e) => {
+        if (e.code === "Enter")
+            sendButton.click();
     }
 }
