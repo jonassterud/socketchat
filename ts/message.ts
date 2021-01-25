2647db902571528c8765f6be95326599184ea6f8
Jonas Sterud <57707537+jonassterud@users.noreply.github.com>
2021-01-25 15:36:03 +0100

    feat: improve styling

--- ts/message.ts
+++ ts/message.ts
@@ -1,11 +1,13 @@
 class Message {
     author: string;
     message: string;
+    color: string;
     time: string;
 
-    constructor (author: string, message: string) {
+    constructor (author: string, message: string, color: string) {
         this.author = author;
         this.message = message;
+        this.color = color;
         this.time = new Date().toTimeString().slice(0, 8);
     }
 }
